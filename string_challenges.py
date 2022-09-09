d362ba170d39cf8f195c17f61726eb16786a9542
Yury <night1ps@gmail.com>
2022-09-10 01:54:28 +0300

    string ex done

--- string_challenges.py
+++ string_challenges.py
@@ -1,3 +1,5 @@
+from collections import Counter
+from itertools import count
 # Вывести последнюю букву в слове
 word = 'Архангельск'
 print(word[-1])
@@ -5,13 +7,17 @@ print(word[-1])
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
+print(Counter(word.lower())['а'])
 
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-# ???
-
+letters = 'аеёоуыэюя'
+count = 0
+for letter in word:
+    if letter in letters: count+=1
+else:
+    print(f'Количество класных букв {count}')
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
@@ -20,9 +26,15 @@ print(len(sentence.split()))
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+for word in sentence.split():
+    print(word[0])
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+lenght = 0
+for word in sentence.split():
+    lenght+= len(word)
+else:
+    print(f'Средняя длина слова в предложении {lenght//len(sentence.split())}')
+
